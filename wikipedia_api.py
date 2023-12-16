bec87fe83cc68ec97022a579af103f82b83d2fd1
am <am@am2.am2.virtualbox.org>
2023-12-16 20:24:10 +0300

    Added wikipedia_api.py
    Former-commit-id: e084890b14d00a6a6a97dfa6273d82ab3d69d63c
    Former-commit-id: 8ba57d5f5618997ecac2871bd93c1babf1a99ee6

---
+++ wikipedia_api.py
@@ -0,0 +1,37 @@
+import wikipedia
+from bs4 import BeautifulSoup as bs
+
+wikipedia.set_lang("ru")
+
+#Принимает список поиска
+#Возвращает список словарей {'find':строка поиска,'summary': краткое содержание,
+#                            'latitude': широта, 'longitude': долгота, '':}
+def getWikipedia(pList):
+
+  result = []
+  for i in pList:
+
+    latitude, longitude = '', ''
+
+    get = wikipedia.search(i)
+    if not get is None:
+
+      summary = wikipedia.summary(get[0])
+
+      page  = wikipedia.page(get[0])
+      soup = bs(page.html(), 'html.parser')
+      find = soup.find("a", {"class": "mw-kartographer-maplink"})
+      if not find is None:
+         latitude =  find['data-lat']
+         longitude = find['data-lon']
+    else:
+      summary = 'Ничего не найдено'
+
+    res = {'find':i,
+             'summary':summary,
+             'latitude':latitude,
+             'longitude':longitude,
+             }
+    result.append(res)
+
+  return result
